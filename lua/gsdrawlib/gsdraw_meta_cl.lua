edea08035e12fefe641f3a56e8ac4a6eeba2e13f
TeraByte <greg.tech.96@mail.ru>
2020-05-02 19:07:31 +0300

    Update gsdraw_meta_cl.lua

--- lua/gsdrawlib/gsdraw_meta_cl.lua
+++ lua/gsdrawlib/gsdraw_meta_cl.lua
@@ -1,14 +1,22 @@
 local panel = FindMetaTable("Panel")
 -- FUNCTION (Easy menu paint)
 function panel:GSMenuPaint(color, soft)
+	self.PaintedColor = color
+	self.PaintedSoft = soft
     function self:Paint(w,h)
-        draw.RoundedBox(soft, 0,0,w, h, color)
-    end 
+        draw.RoundedBox(soft, 0, 0, w, h, color)
+    end
 end
 -- FUNCTION (Make panel invisible)
-function panel:GSMakeInvisible()
-    function self:Paint(w,h)
-    end
+function panel:GSMakeInvisible(bool)
+	if bool then
+	    function self:Paint(w,h)
+	    end
+	else
+		function self:Paint(w,h)
+	        draw.RoundedBox(self.PaintedSoft, 0, 0, w, h, self.PaintedColor)
+	    end
+	end
 end
 -- FUNCTION (Close menu when player die)
 function panel:GSSUDeath()
@@ -32,15 +40,20 @@ function panel:GSDrawTextPanel(m_color, soft, t_color, text, font, alig)
     function self:Paint(w,h)
         draw.RoundedBox(soft, 0,0,w, h, m_color)
     end
+
     if alig == nil then
         alig = 5
     end
+
+	self.TextPanelColor = m_color
+	self.TextPanelSoft = soft
+
     local title = vgui.Create( "DLabel", self )
     title:SetText( text )
     title:SetTextColor(t_color)
     title:SetFont(font)
     title:SizeToContents()
-    title.id = 1
+    title.IsTextPanel = true
     title.Think = function()
         title:SizeToContents()
         title:SetSize(self:GetWide(),self:GetTall())
@@ -50,7 +63,7 @@ end
 -- FUNCTION (Get text from text panel)
 function panel:GSGetTextPanelText()
     for _,v in pairs(self:GetChildren()) do
-        if v.id == 1 then
+        if v.IsTextPanel then
             return v
         end
     end
@@ -67,7 +80,7 @@ function panel:GSDrawScrollBar(m_color, g_color, m_soft, btn_up, btn_down)
     end
 
     local d_scroll = self:GetVBar()
-        
+
     function d_scroll:Paint( w, h )
         draw.RoundedBox( 0, ScrW() * 0.005, 0, ScrW() * 0.005, h, m_color )
     end
@@ -86,7 +99,7 @@ function panel:GSDrawScrollBar(m_color, g_color, m_soft, btn_up, btn_down)
 end
 -- FUNCTION (Setup autocontents)
 function panel:GSAutoContents()
-    local text_pnl = self:GetTextPanelText()
+    local text_pnl = self:GSGetTextPanelText()
     if text_pnl != nil then
         self.Think = function()
             self:SetSize(self:GetWide(), text_pnl:GetTall())
@@ -100,4 +113,27 @@ function panel:GSMakeNiceImage(path, padding)
     niceImage:Dock(FILL)
 
     self:DockPadding(padding[1], padding[2], padding[3], padding[4])
+end
+-- FUNCTION (Make panel hover)
+function panel:GSMakeHover(m_color, text_color, text, alig, x, y, font, smooth)
+  local text_panel = self:GSGetTextPanelText()
+  if not x or not y then
+    x, y = text_panel:GetPos()
+  end
+  self:GSMenuPaint(m_color, smooth)
+  text_panel:SetText(text)
+  text_panel:SetTextColor(text_color)
+  text_panel:SetFont(font)
+  text_panel:SetPos(x, y)
+  text_panel.Think = function()
+      text_panel:SizeToContents()
+      text_panel:SetSize(self:GetWide(),self:GetTall())
+      text_panel:SetContentAlignment(alig)
+  end
+end
+-- FUNCTION (Make true sound)
+function panel:GSSetupHoverSound(sound)
+  self.OnCursorEntered = function()
+    LocalPlayer():EmitSound(sound)
+  end
 end
